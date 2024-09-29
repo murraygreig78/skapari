4ad2a05788c264afd3eeff18fe72b35040c1cf7b
murraygreig <murray@dailymaverick.co.za>
2024-09-29 15:36:20 +0200

    added all projects

---
+++ south-africa-project-pending-1.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<!-- saved from url=(0063)http://localhost/skapari/project/south-africa-project-pending-1 -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Skapari - SOUTH AFRICA: Project Pending </title>
+    <link rel="icon" type="image/png" sizes="16x16" href="http://localhost/skapari/themes/skapari/assets/icons/Icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="http://localhost/skapari/themes/skapari/assets/icons/Icon-72.png">
+    <link rel="stylesheet" href="./south-africa-project-pending-1_files/styles.css">
+    <link href="./south-africa-project-pending-1_files/css" rel="stylesheet">
+<style class="fslightbox-styles">.fslightbox-absoluted{position:absolute;top:0;left:0}.fslightbox-fade-in{animation:fslightbox-fade-in .25s cubic-bezier(0,0,.7,1)}.fslightbox-fade-out{animation:fslightbox-fade-out .25s ease}.fslightbox-fade-in-strong{animation:fslightbox-fade-in-strong .25s cubic-bezier(0,0,.7,1)}.fslightbox-fade-out-strong{animation:fslightbox-fade-out-strong .25s ease}@keyframes fslightbox-fade-in{from{opacity:.65}to{opacity:1}}@keyframes fslightbox-fade-out{from{opacity:.35}to{opacity:0}}@keyframes fslightbox-fade-in-strong{from{opacity:.3}to{opacity:1}}@keyframes fslightbox-fade-out-strong{from{opacity:1}to{opacity:0}}.fslightbox-cursor-grabbing{cursor:grabbing}.fslightbox-full-dimension{width:100%;height:100%}.fslightbox-open{overflow:hidden;height:100%}.fslightbox-flex-centered{display:flex;justify-content:center;align-items:center}.fslightbox-opacity-0{opacity:0!important}.fslightbox-opacity-1{opacity:1!important}.fslightbox-scrollbarfix{padding-right:17px}.fslightbox-transform-transition{transition:transform .3s}.fslightbox-container{font-family:Arial,sans-serif;position:fixed;top:0;left:0;background:linear-gradient(rgba(30,30,30,.9),#000 1810%);z-index:1000000000;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;-webkit-tap-highlight-color:transparent}.fslightbox-container *{box-sizing:border-box}.fslightbox-svg-path{transition:fill .15s ease;fill:#ddd}.fslightbox-nav{height:45px;width:100%;position:absolute;top:0;left:0}.fslightbox-slide-number-container{display:flex;justify-content:center;align-items:center;position:relative;height:100%;font-size:15px;color:#d7d7d7;z-index:0;max-width:55px;text-align:left}.fslightbox-slide-number-container .fslightbox-flex-centered{height:100%}.fslightbox-slash{display:block;margin:0 5px;width:1px;height:12px;transform:rotate(15deg);background:#fff}.fslightbox-toolbar{position:absolute;z-index:3;right:0;top:0;height:100%;display:flex;background:rgba(35,35,35,.65)}.fslightbox-toolbar-button{height:100%;width:45px;cursor:pointer}.fslightbox-toolbar-button:hover .fslightbox-svg-path{fill:#fff}.fslightbox-slide-btn-container{display:flex;align-items:center;padding:12px 12px 12px 6px;position:absolute;top:50%;cursor:pointer;z-index:3;transform:translateY(-50%)}@media (min-width:476px){.fslightbox-slide-btn-container{padding:22px 22px 22px 6px}}@media (min-width:768px){.fslightbox-slide-btn-container{padding:30px 30px 30px 6px}}.fslightbox-slide-btn-container:hover .fslightbox-svg-path{fill:#f1f1f1}.fslightbox-slide-btn{padding:9px;font-size:26px;background:rgba(35,35,35,.65)}@media (min-width:768px){.fslightbox-slide-btn{padding:10px}}@media (min-width:1600px){.fslightbox-slide-btn{padding:11px}}.fslightbox-slide-btn-container-previous{left:0}@media (max-width:475.99px){.fslightbox-slide-btn-container-previous{padding-left:3px}}.fslightbox-slide-btn-container-next{right:0;padding-left:12px;padding-right:3px}@media (min-width:476px){.fslightbox-slide-btn-container-next{padding-left:22px}}@media (min-width:768px){.fslightbox-slide-btn-container-next{padding-left:30px}}@media (min-width:476px){.fslightbox-slide-btn-container-next{padding-right:6px}}.fslightbox-down-event-detector{position:absolute;z-index:1}.fslightbox-slide-swiping-hoverer{z-index:4}.fslightbox-invalid-file-wrapper{font-size:22px;color:#eaebeb;margin:auto}.fslightbox-video{object-fit:cover}.fslightbox-youtube-iframe{border:0}.fslightbox-loader{display:block;margin:auto;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:67px;height:67px}.fslightbox-loader div{box-sizing:border-box;display:block;position:absolute;width:54px;height:54px;margin:6px;border:5px solid;border-color:#999 transparent transparent transparent;border-radius:50%;animation:fslightbox-loader 1.2s cubic-bezier(.5,0,.5,1) infinite}.fslightbox-loader div:nth-child(1){animation-delay:-.45s}.fslightbox-loader div:nth-child(2){animation-delay:-.3s}.fslightbox-loader div:nth-child(3){animation-delay:-.15s}@keyframes fslightbox-loader{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}.fslightbox-source{position:relative;z-index:2;opacity:0}</style></head>
+<body class="skapari">
+<div class="wrapper">
+    <header class="header ">
+        <nav class="navbar" id="main-nav">
+    <!-- Collapse button -->
+    <button class="navbar-toggler third-button ml-auto hidden-sm-up float-xs-right collapsed" type="button" data-toggle="collapse" data-target="#topnavigation" aria-controls="topnavigation" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="animated-icon3"><span></span><span></span><span></span></div>
+    </button>
+    <!-- Collapsible content -->
+    <div class="navbar-collapse collapse" id="topnavigation" style="">
+        <div class="overlay-content">
+            <!-- Links -->
+            <ul class="mainnav navbar-nav mr-auto">
+                <li class="nav-item inactive">
+                    <a class="nav-link " href="https://skapari.co.za/">home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="http://localhost/skapari/project/about">about</a>
+                </li>
+                <li class="nav-item inactive">
+                    <a class="nav-link" href="http://localhost/skapari/project/projects">our work</a>
+                </li>
+                <li class="nav-item inactive">
+                    <a class="nav-link" href="http://localhost/skapari/project/curated">curated</a>
+                </li>
+                <li class="nav-item inactive">
+                    <a class="nav-link" href="http://localhost/skapari/project/contact">contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- Collapsible content -->
+</nav>    </header>
+    <div class="container my-4">
+    </div>
+    <section class="container">
+        <div class="row">
+            <div class="col-md-10 offset-md-1 heading">
+                                <h3 class="skapari-heading  Hospitality">Hospitality<span>  SOUTH AFRICA:  Project Pending</span>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <p class="well"></p>
+                <div class="row">
+                                                            
+                                        <div class="col-lg-12 ">
+                        <div class="bg-image hover-overlay">
+                            <a href="http://localhost/skapari/storage/app/uploads/public/64d/4ad/ae0/thumb_107_1440_900_0_0_auto.jpg" data-fslightbox="">
+                                <img src="./south-africa-project-pending-1_files/thumb_107_920_520_0_0_auto.jpg" class="w-100">
+                            </a>
+                        </div>
+                    </div>
+                                                            
+                                        <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
+                        <div class="bg-image hover-overlay">
+                            <a href="http://localhost/skapari/storage/app/uploads/public/64d/4ad/a7c/64d4ada7c140e278412538.jpg" data-fslightbox="">
+                                <img src="./south-africa-project-pending-1_files/thumb_106_300_200_0_0_auto.jpg" class="w-100">
+                            </a>
+                        </div>
+                    </div>
+                                                        </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 offset-md-1 text-right">
+
+                <a class="nav-link" href="http://localhost/skapari/projects">← back to our work</a>
+            </div>
+        </div>
+    </section>
+</div>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 m-auto h-100 text-center">
+                <ul class="list-inline mb-2">
+                    <li class="list-inline-item text-muted small">© skapari 2018. All Rights Reserved.</li>
+                    <li class="list-inline-item"><span>⋅</span></li>
+                    <li class="list-inline-item"><a href="http://localhost/skapari/project/terms" class="text-muted small">Terms of Use</a></li>
+                    <li class="list-inline-item"><span>⋅</span></li>
+                    <li class="list-inline-item"><a href="http://localhost/skapari/project/privacy-policy" class="text-muted small">Privacy Policy</a>
+                    </li>
+                    <li class="list-inline-item"><span>⋅</span></li>
+                    <li class="list-inline-item"><a href="https://www.instagram.com/interiorsbyskapari/" target="_blank" class="text-muted small">https://www.instagram.com/interiorsbyskapari/</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer><script type="text/javascript" src="./south-africa-project-pending-1_files/7d727397c5ffe6bae5dae799aad9a6c5-1701685306.js">
+</script>
+
+</body></html>
