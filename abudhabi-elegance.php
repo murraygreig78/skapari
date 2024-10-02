0f43d352f48b8b22f1cf79a54aa6110b58e6a63a
murraygreig <murray@dailymaverick.co.za>
2024-10-02 07:26:24 +0200

    refactored abu-dhabi-elegance

--- abudhabi-elegance.php
+++ abudhabi-elegance.php
@@ -21,63 +21,63 @@
         </div>
         <div class="row">
             <div class="col-md-10 offset-md-1">
-                <p class="well"><p dir="ltr">ABU DHABI : elegance</p>
+                <p class="well"><p dir="ltr">ABU DHABI: elegance</p>
 
                 <p>To create intimate and layered interiors where one can unwind and relax. Rich and luxurious... Textured, intimate and refined... Create interiors everyone wants &nbsp; &nbsp;</p></p>
                 <div class="row">
 
                     <div class="col-lg-12 ">
                         <div class="bg-image hover-overlay">
-                            <a href="/uploads/public/5fc/d2f/2f2/thumb_52_1440_900_0_0_auto.jpg" data-fslightbox>
-                                <img src="/uploads/public/5fc/d2f/2f2/thumb_52_920_520_0_0_auto.jpg" class="w-100"/>
+                            <a href="assets/images/abu-dhabi-elegance/abu-dhabi-elegance-1.jpg" data-fslightbox>
+                                <img src="assets/images/abu-dhabi-elegance/abu-dhabi-elegance-1.jpg" class="w-100"/>
                             </a>
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
                         <div class="bg-image hover-overlay">
-                            <a href="/uploads/public/5fc/d2f/2f8/5fcd2f2f8d071522656209.jpg" data-fslightbox>
-                                <img src="/uploads/public/5fc/d2f/2f8/thumb_53_300_200_0_0_auto.jpg" class="w-100"/>
+                            <a href="assets/images/abu-dhabi-elegance/abu-dhabi-elegance-2.jpg" data-fslightbox>
+                                <img src="assets/images/abu-dhabi-elegance/abu-dhabi-elegance-2.jpg" class="w-100"/>
                             </a>
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
                         <div class="bg-image hover-overlay">
-                            <a href="/uploads/public/5fc/d2f/329/5fcd2f329e86d122736389.jpg" data-fslightbox>
-                                <img src="/uploads/public/5fc/d2f/329/thumb_54_300_200_0_0_auto.jpg" class="w-100"/>
+                            <a href="assets/images/abu-dhabi-elegance/ABU-DHABI-elegance-3.jpg" data-fslightbox>
+                                <img src="assets/images/abu-dhabi-elegance/ABU-DHABI-elegance-3.jpg" class="w-100"/>
                             </a>
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
                         <div class="bg-image hover-overlay">
-                            <a href="/uploads/public/5fc/d2f/344/5fcd2f344ab87733720339.jpg" data-fslightbox>
-                                <img src="/uploads/public/5fc/d2f/344/thumb_55_300_200_0_0_auto.jpg" class="w-100"/>
+                            <a href="/assets/images/abu-dhabi-elegance/abu-dhabi-elegance-4.jpg" data-fslightbox>
+                                <img src="/assets/images/abu-dhabi-elegance/abu-dhabi-elegance-4.jpg" class="w-100"/>
                             </a>
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
                         <div class="bg-image hover-overlay">
-                            <a href="/uploads/public/5fc/d2f/375/5fcd2f375afa5279310186.jpg" data-fslightbox>
-                                <img src="/uploads/public/5fc/d2f/375/thumb_56_300_200_0_0_auto.jpg" class="w-100"/>
+                            <a href="/assets/images/abu-dhabi-elegance/abu-dhabi-elegance-5.jpg" data-fslightbox>
+                                <img src="/assets/images/abu-dhabi-elegance/abu-dhabi-elegance-5.jpg" class="w-100"/>
                             </a>
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
                         <div class="bg-image hover-overlay">
-                            <a href="/uploads/public/5fc/d2f/391/5fcd2f39195ac195527817.jpg" data-fslightbox>
-                                <img src="/uploads/public/5fc/d2f/391/thumb_57_300_200_0_0_auto.jpg" class="w-100"/>
+                            <a href="/assets/images/abu-dhabi-elegance/abu-dhabi-elegance-6.jpg" data-fslightbox>
+                                <img src="/assets/images/abu-dhabi-elegance/abu-dhabi-elegance-6.jpg" class="w-100"/>
                             </a>
                         </div>
                     </div>
 
                     <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
                         <div class="bg-image hover-overlay">
-                            <a href="/uploads/public/5fc/d2f/3a7/5fcd2f3a711ad950618442.jpg" data-fslightbox>
-                                <img src="/uploads/public/5fc/d2f/3a7/thumb_58_300_200_0_0_auto.jpg" class="w-100"/>
+                            <a href="/assets/images/abu-dhabi-elegance/abu-dhabi-elegance-7.jpg" data-fslightbox>
+                                <img src="/assets/images/abu-dhabi-elegance/abu-dhabi-elegance-7.jpg" class="w-100"/>
                             </a>
                         </div>
                     </div>
