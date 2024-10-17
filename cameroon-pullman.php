b65f900d320a9f783f720aa7f6f48bfcfa109108
murraygreig <murray@dailymaverick.co.za>
2024-10-17 12:44:16 +0200

    added cameroon-pullman.php

---
+++ cameroon-pullman.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<!-- saved from url=(0054)http://localhost/skapari/project/sal-cape-verde -->
+<html lang="en">
+<head><title>Skapari - Cameroon: Pullman </title>
+    <?php
+    include('head.php'); ?>
+</head>
+<body class="skapari">
+<div class="wrapper">
+    <header class="header ">
+        <?php
+        include('nav.php'); ?>    </header>
+    <div class="container my-4">
+    </div>
+    <section class="container">
+        <div class="row">
+            <div class="col-md-10 offset-md-1 heading">
+                <h3 class="skapari-heading  Hospitality">Hospitality<span>  Cameroon: Pullman</span>
+                </h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <p class="well"></p>
+                <div class="row">
+
+                    <div class="col-lg-12 ">
+                        <div class="bg-image hover-overlay">
+                            <a href="assets/images/cameroon-pullman/cameroon-pullman2.jpg" data-fslightbox="">
+                                <img src="assets/images/cameroon-pullman/cameroon-pullman2.jpg" class="w-100">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
+                        <div class="bg-image hover-overlay">
+                            <a href="assets/images/cameroon-pullman/cameroon-pullman1.jpg" data-fslightbox="">
+                                <img src="assets/images/cameroon-pullman/cameroon-pullman1.jpg" class="w-100">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
+                        <div class="bg-image hover-overlay">
+                            <a href="assets/images/cameroon-pullman/cameroon-pullman3.jpg" data-fslightbox="">
+                                <img src="assets/images/cameroon-pullman/cameroon-pullman3.jpg" class="w-100">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
+                        <div class="bg-image hover-overlay">
+                            <a href="assets/images/cameroon-pullman/cameroon-pullman4.jpg" data-fslightbox="">
+                                <img src="assets/images/cameroon-pullman/cameroon-pullman4.jpg" class="w-100">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
+                        <div class="bg-image hover-overlay">
+                            <a href="assets/images/cameroon-pullman/cameroon-pullman5.jpg" data-fslightbox="">
+                                <img src="assets/images/cameroon-pullman/cameroon-pullman5.jpg" class="w-100">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
+                        <div class="bg-image hover-overlay">
+                            <a href="assets/images/cameroon-pullman/cameroon-pullman6.jpg" data-fslightbox="">
+                                <img src="assets/images/cameroon-pullman/cameroon-pullman6.jpg" class="w-100">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
+                        <div class="bg-image hover-overlay">
+                            <a href="assets/images/cameroon-pullman/cameroon-pullman7.jpg" data-fslightbox="">
+                                <img src="assets/images/cameroon-pullman/cameroon-pullman7.jpg" class="w-100">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-6 mt-4">
+                        <div class="bg-image hover-overlay">
+                            <a href="assets/images/cameroon-pullman/cameroon-pullman8.jpg" data-fslightbox="">
+                                <img src="assets/images/cameroon-pullman/cameroon-pullman8.jpg" class="w-100">
+                            </a>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 offset-md-1 text-right">
+
+                <a class="nav-link p-2 m-5" href="projects">← back to our work</a>
+            </div>
+        </div>
+    </section>
+</div>
+<?php
+include('footer.php'); ?>
