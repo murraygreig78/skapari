8e8b8b941b4d3aeac089fb4b90428ceec550afe0
murraygreig <murray@dailymaverick.co.za>
2024-10-17 11:03:29 +0200

    added ga4

--- head.php
+++ head.php
@@ -4,3 +4,13 @@
 <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/Icon-72.png">
 <link rel="stylesheet" href="/assets/css/styles.css">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:100,300,600&display=swap" rel="stylesheet">
+<!--<script id="usercentrics-cmp" src="https://app.usercentrics.eu/browser-ui/latest/loader.js" data-settings-id="PqQBefBrFMqKqM" async></script>-->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1EPX2FR6N0"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-1EPX2FR6N0');
+</script>
