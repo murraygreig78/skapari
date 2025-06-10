3fdd2228ba750bd132dc3e75ca5055b02cdfd1e0
murraygreig <murray@dailymaverick.co.za>
2025-06-10 08:30:27 +0200

    astro up and running

---
+++ nasty-nebula/src/assets/images/index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Interiors by Skapari</title>
+    <meta name="description" content="Skapari = Creators. Interior design specialists with a focus on Conceptual Design and Creative Direction for Hospitality. Our portfolio that extends from Europe to Africa, the Middle East and Indian Ocean Islands.">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,600&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="skapari-rune.ico" sizes="16x16">
+    <style>
+        body, html {
+            height: 100%;
+            margin: 0;
+            font: 400 15px/1.8 'Montserrat', sans-serif;
+            color: #777;
+        }
+
+        .bgimg-1, .bgimg-2, .bgimg-3 {
+            position: relative;
+            /*opacity: 0.65;*/
+            background-attachment: fixed;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+
+        }
+
+        .logo-holder {
+            background-color: #0F062F;
+            min-height: 100%;
+            position: relative;
+        }
+
+        .bgimg-1 {
+            background-image: url("TableBay.jpg");
+            min-height: 100%;
+        }
+
+        .bgimg-2 {
+            background-image: url("View1_SelectVilla_Dining_001.jpg");
+            min-height: 100%;
+        }
+
+        .bgimg-3 {
+            background-image: url("View4_Sahl5BdrmVilla_Lounge_001.jpg");
+            min-height: 100%;
+        }
+
+        .bgimg-4 {
+            display: none;
+        }
+
+        @media only screen (min-width: 768px){
+
+            .bgimg-4 {
+                background-image: url("Accor-offices_0005.jpg");
+                min-height: 100%;
+            }
+        }
+
+
+
+        .caption, .logo {
+            position: absolute;
+            left: 0;
+            top: 50%;
+            width: 100%;
+            text-align: center;
+            color: #000;
+        }
+
+        .logo {
+
+            top: 40%;
+
+        }
+
+        .caption span.border {
+            background-color: #111;
+            color: #fff;
+            padding: 18px;
+            font-size: 25px;
+            letter-spacing: 10px;
+        }
+
+        h3 {
+            letter-spacing: 5px;
+            text-transform: uppercase;
+            font: 20px "Montserrat", sans-serif;
+            font-weight: 600;
+            color: #111;
+        }
+
+        .footer-contact {
+            float: left;
+            width: 50%;
+        }
+
+
+
+        /* Turn off parallax scrolling for tablets and phones */
+        @media only screen and (max-device-width: 1024px) {
+            .bgimg-1, .bgimg-2, .bgimg-3 {
+                background-attachment: scroll;
+            }
+        }
+    </style>
+    <!-- Global site tag (gtag.js) - Google Ads: 626455971 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-626455971"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-626455971');
+</script>
+
+</head>
+<body>
+<div class="logo-holder">
+    <div class="logo">
+        <img src="skapari.svg" width="50%">
+    </div>
+</div>
+<div class="bgimg-1">
+</div>
+<div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+    <h3 style="text-align:center;">Skapari = Creators</h3>
+    <p>Interior design specialists with a focus on Conceptual Design and Creative Direction for Hospitality. Our portfolio that extends from Europe to Africa, the Middle East and Indian Ocean Islands.</p>
+</div>
+
+<div class="bgimg-2">
+</div>
+<div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+    <h3 style="text-align:center;">Jacqualin & Riejean</h3>
+    <p>We believe design should be functionally aesthetic, it should embrace and reflect its location. Draw inspiration from its surroundings and history layering it with a contemporary feel and touch of the unexpected.</p>
+</div>
+<div class="bgimg-3">
+</div>
+
+<div class="bgimg-4">
+</div>
+<footer style="text-align: center; ">
+    <div style="margin: 30px;"><img src="skapari-rune.svg" height="150px"></div>
+    <div class="footer-contact" >
+        <h4>Jacqualin Greig </h4>
+        <p>jacky@skapari.co.za </p>
+        <p>+27 (0)83 326 6078  </p>
+    </div>
+    <div class="footer-contact">
+        <h4>Riejean Joubert </h4>
+        <p>riejean@skapari.co.za </p>
+        <p>+27 (0)71 917 6511   </p>
+    </div>
+    <div style="margin-bottom: 50px; clear: both;"></div>
+</footer>
+
+</body>
+</html>
