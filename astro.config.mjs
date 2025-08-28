40072b91585179618d042b06f0a1bc8d09d1e552
MurrayGreig <murray.greig78@gmail.com>
2025-08-28 10:25:53 +0200

    Update astro.config.mjs
    fix path 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://murraygreig78.github.io',
-  base: process.env.ASTRO_BASE_PATH || '/skapari',
+  base: process.env.ASTRO_BASE_PATH || '/skapari/',
   integrations: [svelte()]
 });
